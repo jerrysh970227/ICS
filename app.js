fd2b59cfe812a6245212431d06aff7a3f6c66e32
jerrysh970227 <jerrysh0227@gmail.com>
2024-09-03 00:50:41 +0800

    change

--- app.js
+++ app.js
@@ -48,3 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   });
   
+
+  document.querySelector("nav .burger .inside #burger").addEventListener("change", ev => {
+    document.querySelector("nav .color-change").classList[["add", "remove"][Number(!document.querySelector("nav .burger .inside #burger").checked)]]("show");
+  })
